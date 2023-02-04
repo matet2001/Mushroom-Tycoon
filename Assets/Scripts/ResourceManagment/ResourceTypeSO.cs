d3656787c564fdd3a7c6b4dd5f573ec99273a758
matet2001 <75041284+matet2001@users.noreply.github.com>
2023-02-04 01:13:23 +0100

    Resource Manager Set Up

---
+++ Assets/Scripts/ResourceManagment/ResourceTypeSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "ResourceTypeSO", menuName = "ScriptableObjects/ResourceType")]
+public class ResourceTypeSO : ScriptableObject
+{
+    public string resourceName;
+    public Image resourceImageUI;
+}
