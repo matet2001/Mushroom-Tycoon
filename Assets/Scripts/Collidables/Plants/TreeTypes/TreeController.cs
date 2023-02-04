093f7358733371df378ee211145ed5a091b3a698
Devel0per Marley <49081673+marloss@users.noreply.github.com>
2023-02-04 20:11:56 +0100

    selector -> animáció és forgás

--- Assets/Scripts/Collidables/Plants/TreeTypes/TreeController.cs
+++ Assets/Scripts/Collidables/Plants/TreeTypes/TreeController.cs
@@ -1,14 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TreeController : PlantBase
 {
     
     public ResourceData resourceData;
     [SerializeField] TreeTypeSO treeType;
+    [Space(5f), Header("Selected sprite attributes")]
+    public bool isSelected;
 
+    [Space, Range(1,35f)] public float rotationSpeed;
+    [SerializeField] private Sprite _selectedObjectSprite; //Ez vagy null
+    [SerializeField] private SpriteRenderer selectedSpriteRenderer;
     private SpriteRenderer spriteRenderer;
 
     private int growTime, growLevel;
@@ -17,12 +24,35 @@ public class TreeController : PlantBase
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         SetUpTreeType();
-        
     }
     private void Start()
     {
         ResourceManager.Instance.OnResourceAmountRefresh += Instance_OnResourceAmountRefresh;
     }
+
+    private void Update()
+    {
+        SetSprite(isSelected, _selectedObjectSprite);
+        switch (isSelected)
+        {
+            case true:
+                RotateSelectorSprite(rotationSpeed);
+                break;
+            case false:
+                selectedSpriteRenderer.transform.localEulerAngles = Vector3.zero;
+                break;
+        }
+    }
+
+    private void SetSprite(bool isSelected, Sprite selectedSprite)
+        => selectedSpriteRenderer.sprite = isSelected 
+            ? selectedSprite 
+            : null;
+
+    private void RotateSelectorSprite(float degree)
+        => selectedSpriteRenderer.gameObject.transform.localEulerAngles =
+            new Vector3(0, 0, selectedSpriteRenderer.transform.localEulerAngles.z + degree * Time.deltaTime);
+    
     private void SetUpTreeType()
     {
         spriteRenderer.sprite = treeType.treeSprites[0];
