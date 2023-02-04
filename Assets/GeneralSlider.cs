8f3e7bbad79fa014d5af931023292b0ba7a47b3c
Devel0per Marley <49081673+marloss@users.noreply.github.com>
2023-02-04 19:25:10 +0100

    Slider + ikonok

---
+++ Assets/GeneralSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class GeneralSlider : MonoBehaviour
+{
+    public float sliderProgress;
+    [Space] 
+    public TextMeshProUGUI sliderText;
+    [Space] 
+    public Image slider;
+    public Image sliderIcon;
+
+    private void SliderSetup()
+    {
+        FillSlider();
+    }
+
+    public void SetSliderFill(float currentValue, float maximumValue) 
+        => sliderProgress = currentValue / maximumValue;
+
+    public void SetSliderText(string textContent)
+        => sliderText.text = textContent;
+
+    public void SetSliderIcon(Sprite icon)
+        => sliderIcon.sprite = icon;
+    
+    private void FillSlider() 
+        => slider.fillAmount = sliderProgress;
+}
