edc9c6c4bdc1b06029082b49540d53c78d71df93
Devel0per Marley <49081673+marloss@users.noreply.github.com>
2023-02-04 14:55:28 +0100

    szöveg -> slider.

--- Assets/Scripts/ResourceManagment/ResourceManager.cs
+++ Assets/Scripts/ResourceManagment/ResourceManager.cs
@@ -13,12 +13,20 @@ public class ResourceManager : MonoBehaviour
     private Dictionary<ResourceTypeSO, int> resourceAmount;
     private Dictionary<ResourceTypeSO, int> resourceUsage;
     private Dictionary<ResourceTypeSO, int> resourceGet;
-    private Dictionary<ResourceTypeSO, int> maxResourceAmount;
+    private Dictionary<ResourceTypeSO, int> currentMaximumResource;
+    private Dictionary<ResourceTypeSO, int> maximumResource;
+    private Dictionary<ResourceTypeSO, int> maximumPossibleResource;
 
     public event Action<ResourceTypeSO[], int[]> OnResourceAmountChange;
 
     [SerializeField] float resourceRefreshTime = 3;
     private float resourceRefreshTimeMax;
+    [Space]
+    public List<int> maximumResourceCapacityLimit; //Ennél több resourcet nem lehet összeszedni.
+    [Space] 
+    [SerializeField] List<int> currentMaximumResourceCapacity; //Mennyi a személyes maximum (amikor kevesebb a fa, mint a kapacitás)
+    [SerializeField] List<int> resourceUsageAmount;
+    [SerializeField] List<int> resourceAdditionAmount;
 
     private void Awake()
     {
@@ -39,15 +47,23 @@ public class ResourceManager : MonoBehaviour
         resourceAmount = new Dictionary<ResourceTypeSO, int>();
         resourceUsage = new Dictionary<ResourceTypeSO, int>();
         resourceGet = new Dictionary<ResourceTypeSO, int>();
-        maxResourceAmount = new Dictionary<ResourceTypeSO, int>();
+        currentMaximumResource = new Dictionary<ResourceTypeSO, int>();
+        maximumResource = new Dictionary<ResourceTypeSO, int>();
+        maximumPossibleResource = new Dictionary<ResourceTypeSO, int>();
+
+        int limit = resourceTypes.Length;
+        int i = 0;
 
         foreach (ResourceTypeSO resourceType in resourceTypes)
         {
+            resourceAmount.Add(resourceType, currentMaximumResourceCapacity[i]);
+            resourceUsage.Add(resourceType, resourceUsageAmount[i]);
+            resourceGet.Add(resourceType, resourceAdditionAmount[i]);
+            currentMaximumResource.Add(resourceType, currentMaximumResourceCapacity[i]);
             
-            resourceAmount.Add(resourceType, 50);
-            resourceUsage.Add(resourceType, 5);
-            resourceGet.Add(resourceType, 3);
-            maxResourceAmount.Add(resourceType, 100);
+            maximumResource.Add(resourceType ,currentMaximumResourceCapacity[i]);
+            maximumPossibleResource.Add(resourceType, maximumResourceCapacityLimit[i]);
+            i++;
         }
 
         resourceRefreshTimeMax = resourceRefreshTime;
@@ -118,6 +134,48 @@ public class ResourceManager : MonoBehaviour
         }
 
         return resourceNames;
+    }
+    public int GetResourceCurrentMaximumAmount(ResourceTypeSO resourceType)
+    {
+        return currentMaximumResource[resourceType];
+    }
+    // elérhető maximum
+    public int GetMaximumResourceAmount(ResourceTypeSO resourceType)
+    {
+        return maximumPossibleResource[resourceType];
+    }
+    public int[] GetMaximumResourceAmounts()
+    {
+        var _resourceAmount = new int[3];
+        for (int j = 0; j < maximumResourceCapacityLimit.Count; j++)
+        {
+            _resourceAmount[j] = maximumResourceCapacityLimit[j];
+        }
+
+        return _resourceAmount;
+
+    }
+    //Jelenlegi maximum
+    public int GetCurrentMaximumResourceAmount(ResourceTypeSO resourceType)
+    {
+        return maximumResource[resourceType];
+    }
+    public int[] GetCurrentMaximumResourceAmounts()
+    {
+        var currentResourceAmount = new int[3];
+        var i = 0;
+        foreach (var items in currentMaximumResourceCapacity)
+        {
+            currentResourceAmount[i] = items;
+            i++;
+        }
+
+        return currentResourceAmount;
+    }
+    //jelenlegi resource
+    public int GetResoruceAmount(ResourceTypeSO resourceType)
+    {
+        return resourceAmount[resourceType];
     }
     public int[] GetResourceAmounts()
     {
--- Assets/Scripts/UI/CanvasManager.cs
+++ Assets/Scripts/UI/CanvasManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour
 {
     [SerializeField] Transform[] resourceAmountTransforms;
+    [Space] public Image[] resourceAmountSliders;
 
     private void Start()
     {
@@ -15,18 +16,21 @@ public class CanvasManager : MonoBehaviour
         ResourceManager.Instance.OnResourceAmountChange += Instance_OnResourceAmountChange;
     }
 
-    
-
     private void SetUpResourceAmountUI()
     {
         string[] resourceNames = ResourceManager.Instance.GetResourceNames();
-        int[] resourceAmounts = ResourceManager.Instance.GetResourceAmounts();
+        int[] currentResourceAmounts = ResourceManager.Instance.GetResourceAmounts();
+        int[] currentMaximumResourceAmounts = ResourceManager.Instance.GetCurrentMaximumResourceAmounts();
+        int[] maximumPossibleResourceAmounts = ResourceManager.Instance.GetMaximumResourceAmounts();
 
         for (int i = 0; i < resourceAmountTransforms.Length; i++)
         {
             TextMeshProUGUI[] resourceAmountTexts = resourceAmountTransforms[i].GetComponentsInChildren<TextMeshProUGUI>();
             resourceAmountTexts[0].text = resourceNames[i];
-            resourceAmountTexts[1].text = resourceAmounts[i].ToString();
+            resourceAmountSliders[i].fillAmount = 
+                // ReSharper disable once PossibleLossOfFraction
+                (currentResourceAmounts[i] / currentMaximumResourceAmounts[i]) /
+                                                   maximumPossibleResourceAmounts[i];
         }
     }
     private void RefreshResourceAmountUI()
@@ -35,10 +39,17 @@ public class CanvasManager : MonoBehaviour
     }
     private void Instance_OnResourceAmountChange(ResourceTypeSO[] arg1, int[] resourceAmount)
     {
+        string[] resourceNames = ResourceManager.Instance.GetResourceNames();
+        int[] currentResourceAmounts = ResourceManager.Instance.GetResourceAmounts();
+        int[] currentMaximumResourceAmounts = ResourceManager.Instance.GetCurrentMaximumResourceAmounts();
+        // int[] maximumPossibleResourceAmounts = ResourceManager.Instance.GetMaximumResourceAmounts();
+        
         for (int i = 0; i < resourceAmountTransforms.Length; i++)
         {
             TextMeshProUGUI[] resourceAmountTexts = resourceAmountTransforms[i].GetComponentsInChildren<TextMeshProUGUI>();
-            resourceAmountTexts[1].text = resourceAmount[i].ToString();
+            resourceAmountTexts[0].text = resourceNames[i];
+            resourceAmountSliders[i].fillAmount = 
+               (currentResourceAmounts[i] / currentMaximumResourceAmounts[i]);
         }
     }
 }
